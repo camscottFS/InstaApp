7119bde896af8158a980c4faeb782e726f0c506d
Cam Scott <camscott@fullsail.edu>
2019-06-01 20:44:10 -0500

    register + home blade

--- resources/views/home.blade.php
+++ resources/views/home.blade.php
@@ -8,7 +8,7 @@
       </div>
       <div class="col-lg-9 pt-5">
         <div class="">
-          <h1>InstaAppProfile</h1>
+          <h1>instaprofile</h1>
         </div>
         <div class="d-flex">
           <div class="pr-5">
@@ -21,6 +21,26 @@
             <strong>212</strong> following
           </div>
         </div>
+        <div class="pt-4 font-weight-bold">
+          CamScott.me
+        </div>
+        <div class="">
+          We're a global community of millions of people learning to code together. We're an open source, donor-supported, 501(c)(3) nonprofit.
+        </div>
+        <div class="">
+          <a href="#">www.camscott.me</a>
+        </div>
+      </div>
+    </div>
+    <div class="row pt-5">
+      <div class="col-4">
+        <img src="https://scontent-msp1-1.cdninstagram.com/vp/984e496117837b4e0b36e2ea67ed8492/5D9E5693/t51.2885-15/sh0.08/e35/c3.0.744.744/s640x640/61895701_468968617186490_3920851586775761705_n.jpg?_nc_ht=scontent-msp1-1.cdninstagram.com" class="w-100">
+      </div>
+      <div class="col-4">
+        <img src="https://scontent-msp1-1.cdninstagram.com/vp/984e496117837b4e0b36e2ea67ed8492/5D9E5693/t51.2885-15/sh0.08/e35/c3.0.744.744/s640x640/61895701_468968617186490_3920851586775761705_n.jpg?_nc_ht=scontent-msp1-1.cdninstagram.com" class="w-100">
+      </div>
+      <div class="col-4">
+        <img src="https://scontent-msp1-1.cdninstagram.com/vp/984e496117837b4e0b36e2ea67ed8492/5D9E5693/t51.2885-15/sh0.08/e35/c3.0.744.744/s640x640/61895701_468968617186490_3920851586775761705_n.jpg?_nc_ht=scontent-msp1-1.cdninstagram.com" class="w-100">
       </div>
     </div>
   </div>
