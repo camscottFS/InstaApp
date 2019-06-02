cc5ca516595a0c880d640e276019eeb40812e0e2
Cam Scott <camscott@fullsail.edu>
2019-06-01 20:28:40 -0500

    home + app blade updates

--- resources/views/home.blade.php
+++ resources/views/home.blade.php
@@ -1,23 +1,27 @@
 @extends('layouts.app')
 
 @section('content')
-<div class="container">
-    <div class="row justify-content-center">
-        <div class="col-md-8">
-            <div class="card">
-                <div class="card-header">Dashboard</div>
-
-                <div class="card-body">
-                    @if (session('status'))
-                        <div class="alert alert-success" role="alert">
-                            {{ session('status') }}
-                        </div>
-                    @endif
-
-                    You are logged in!
-                </div>
-            </div>
+  <div class="container">
+    <div class="row">
+      <div class="col-lg-3 p-5">
+        <img src="https://scontent-msp1-1.cdninstagram.com/vp/79938071ad6a79c5863e8e3da4b17004/5D8CE438/t51.2885-19/s150x150/22709172_932712323559405_7810049005848625152_n.jpg?_nc_ht=scontent-msp1-1.cdninstagram.com" class="rounded-circle">
+      </div>
+      <div class="col-lg-9 pt-5">
+        <div class="">
+          <h1>InstaAppProfile</h1>
         </div>
+        <div class="d-flex">
+          <div class="pr-5">
+            <strong>153</strong> posts
+          </div>
+          <div class="pr-5">
+            <strong>23k</strong> followers
+          </div>
+          <div class="pr-5">
+            <strong>212</strong> following
+          </div>
+        </div>
+      </div>
     </div>
-</div>
+  </div>
 @endsection
