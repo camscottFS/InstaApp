31d239dae08f699375c5a433b9c4db99e24d0dd6
Cam Scott <camscott@fullsail.edu>
2019-06-11 17:21:33 -0500

    image resizing

--- app/Http/Controllers/PostsController.php
+++ app/Http/Controllers/PostsController.php
@@ -3,6 +3,7 @@
 namespace App\Http\Controllers;
 
 use Illuminate\Http\Request;
+use Intervention\Image\Facades\Image;
 
 class PostsController extends Controller
 {
@@ -12,17 +13,22 @@ class PostsController extends Controller
 		$this->middleware('auth');
 	}
 
-    public function create(){
+    public function create()
+    {
       return view('posts.create');
     }
 
-    public function store(){
+    public function store()
+    {
     	$data = request()->validate([
     		'caption' => 'required',
     		'image' => 'required|image',
     	]);
 
     	$imagePath = request('image')->store('uploads', 'public');
+
+    	$image = Image::make(public_path('storage/{$imagePath}'))->fit(1200, 1200);
+    	$image->save();
 
     	auth()->user()->posts()->create([
     		'caption' => $data['caption'],
